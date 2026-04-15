398b72baf6f33c2accb4783d0ebbb75c99cc5967
fruittea-ai <mhmdrosyid19@gmail.com>
2026-04-15 20:15:52 +0700

    membuat DAO

--- app/src/main/java/com/example/unscramble/data/WordDao.kt
+++ app/src/main/java/com/example/unscramble/data/WordDao.kt
@@ -7,13 +7,12 @@ import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
 
-
 @Dao
 interface WordDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(word: Word)
 
-    @Query("SELECT * FROM words")
-    suspend fun getAllWords(): Flow<List<String>>
+    @Query("SELECT word FROM words")
+    fun getAllWords(): Flow<List<String>>
 
 }
